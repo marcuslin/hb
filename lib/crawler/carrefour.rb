c88ed052448cdf512fbe095c8f0859b3b6af6cd6
marcus lin <marcuslin0724@gmail.com>
2018-03-11 16:59:41 +0800

    fix carrefour indent

--- lib/crawler/carrefour.rb
+++ lib/crawler/carrefour.rb
@@ -6,55 +6,55 @@ module Crawler
       attr_reader :key_word, :browser
 
       def initialize(key_word)
-				poltergeist_driver
-				@key_word = url_encode(key_word)
+        poltergeist_driver
+        @key_word = url_encode(key_word)
       end
 
       def crawl
-				prod_count = 0
+        prod_count = 0
 
-				visit "#{SITE_URL}search?key=#{key_word}"
-				sleep 10
-				results = to_hash_format(fetch_items)
+        visit "#{SITE_URL}search?key=#{key_word}"
+        sleep 10
+        results = to_hash_format(fetch_items)
 
-				return results
+        return results
       end
 
       private
 
-			def fetch_items(previous_count = 0, execute_num = 1)
-				items = ''
-				current_count = 0
-				execute_script('window.scroll(0,999999);')
-				sleep 5
+      def fetch_items(previous_count = 0, execute_num = 1)
+        items = ''
+        current_count = 0
+        execute_script('window.scroll(0,999999);')
+        sleep 5
 
-				within(".items-block") do
-					items = all('.item-product')
-				end
+        within(".items-block") do
+          items = all('.item-product')
+        end
 
-				current_count += items.size
+        current_count += items.size
 
-				return items if previous_count == current_count
-				
-				previous_count = current_count
-				execute_num += 1
+        return items if previous_count == current_count
 
-				fetch_items(previous_count, execute_num)
-			end
+        previous_count = current_count
+        execute_num += 1
 
-			def to_hash_format(items)
-				result = []
+        fetch_items(previous_count, execute_num)
+      end
 
-				items.each do |item|
-					puts item.find('.item-name').text
-					result << { item_name: item.find('.item-name').text,
-											item_price: item.find('.discount-price').text,
-											img_src: item.find('.label-wrap .center-block')['src']
-										}					
-				end;0
+      def to_hash_format(items)
+        result = []
 
-				return result
-			end
-		end
+        items.each do |item|
+          puts item.find('.item-name').text
+          result << { item_name: item.find('.item-name').text,
+                      item_price: item.find('.discount-price').text,
+                      img_src: item.find('.label-wrap .center-block')['src']
+          }					
+        end;0
+
+        return result
+      end
+    end
   end
 end
