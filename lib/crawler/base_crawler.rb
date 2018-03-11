07d22d6016b410794e79fa4e02e9c4f6db38a731
marcus lin <marcuslin0724@gmail.com>
2018-03-11 16:56:17 +0800

    Add crawler - add base_crawler and capybara settings - add and finish carrefour crawler - add rt_mart crawler

---
+++ lib/crawler/base_crawler.rb
@@ -0,0 +1,52 @@
+require 'uri'
+require 'capybara/rails'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+module Crawler
+	class BaseCrawler
+		include Capybara::DSL
+    include ERB::Util
+
+		def initialize
+		end
+
+		def poltergeist_driver(image_enabled = false)
+			phantomjs_options = []
+			if image_enabled
+				phantomjs_options = [
+					'--proxy-type=none',
+					'--ignore-ssl-errors=yes',
+					'--ssl-protocol=any',
+					'--web-security=false'
+				]
+			else
+				phantomjs_options = [
+					'--proxy-type=none',
+					'--load-images=no',
+					'--ignore-ssl-errors=yes',
+					'--ssl-protocol=any',
+					'--web-security=false'
+				]
+			end
+			driver_options = {
+				timeout: 30,
+				window_size: [1280, 1440],
+				js_errors: false,
+				phantomjs_options: phantomjs_options
+			}
+			Capybara.register_driver :poltergeist do |app|
+				Capybara::Poltergeist::Driver.new(app, driver_options)
+			end
+			Capybara.javascript_driver = :poltergeist
+			Capybara.current_driver = Capybara.javascript_driver
+			Capybara::Session.new(:poltergeist)
+			page.driver.headers =
+				{"user-agent": 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36.',
+		 "accept-language": 'zh-tw'
+			}
+		end
+	end
+end
+
+
---
+++ lib/crawler/carrefour.rb
@@ -0,0 +1,60 @@
+module Crawler
+  module Carrefour
+    SITE_URL = Figaro.env.carrefour_url
+
+    class Base < BaseCrawler
+      attr_reader :key_word, :browser
+
+      def initialize(key_word)
+				poltergeist_driver
+				@key_word = url_encode(key_word)
+      end
+
+      def crawl
+				prod_count = 0
+
+				visit "#{SITE_URL}search?key=#{key_word}"
+				sleep 10
+				results = to_hash_format(fetch_items)
+
+				return results
+      end
+
+      private
+
+			def fetch_items(previous_count = 0, execute_num = 1)
+				items = ''
+				current_count = 0
+				execute_script('window.scroll(0,999999);')
+				sleep 5
+
+				within(".items-block") do
+					items = all('.item-product')
+				end
+
+				current_count += items.size
+
+				return items if previous_count == current_count
+				
+				previous_count = current_count
+				execute_num += 1
+
+				fetch_items(previous_count, execute_num)
+			end
+
+			def to_hash_format(items)
+				result = []
+
+				items.each do |item|
+					puts item.find('.item-name').text
+					result << { item_name: item.find('.item-name').text,
+											item_price: item.find('.discount-price').text,
+											img_src: item.find('.label-wrap .center-block')['src']
+										}					
+				end;0
+
+				return result
+			end
+		end
+  end
+end
