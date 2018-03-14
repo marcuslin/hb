eb3ae725b1b263315ba559d4d81ccec296bfc1d2
marcus lin <marcuslin0724@gmail.com>
2018-03-15 02:07:19 +0800

    add test for api

--- spec/api/v1/search_item_spec.rb
+++ spec/api/v1/search_item_spec.rb
@@ -1,28 +1,48 @@
 require 'rails_helper'
 
 describe V1::SearchItem do
-  context 'POST /api/v1/search' do
+  let(:key_word) { '雞蛋' }
+  let(:results) { crawl_results }
+
+  before do
+    allow_any_instance_of(Crawler::Carrefour::Base).to receive(:call)
+      .and_return(results)
+
+    allow_any_instance_of(Crawler::RtMart::Base).to receive(:call)
+      .and_return(results)
+  end
+
+  describe 'POST /api/v1/search' do
     context 'with valid key word' do
-      subject { post "/api/v1/search", params: { key_word: 'test' } }
-      
-      before do
-        allow(Crawler::Carrefour::Base.new('test')).to receive(:crawl).and_return({ test: '123' })
-        allow(Crawler::RtMart::Base.new('test')).to receive(:crawl).and_return({ test: '123' })
+      subject { post "/api/v1/search", params: { key_word: key_word } }
+
+      before { subject }
+
+      it 'returns 201' do
+        expect(response).to have_http_status 201
       end
 
-      it 'returns search results' do
-        subject
+      it 'returns search results with key_word as key' do
+        expect(json_response).to have_key(key_word.to_sym)
+      end
+
+      it 'returns search results with rt_mart_results key' do
+        expect(json_response[key_word.to_sym]).to have_key(:rt_mart_results)
+      end
+
+      it 'returns search results with carrefour_results key' do
+        expect(json_response[key_word.to_sym]).to have_key(:carrefour_results)
       end
     end
-    #
-    # context 'with invalid key word' do
-    #   subject { post "/api/v1/search", params: { key_word: '' } }
-    #
-    #   it 'returns search results' do
-    #     # expect(subject)
-    #     # subject
-    #   end
-    # end
 
+    context 'with invalid key word' do
+      subject { post "/api/v1/search", params: { key_word: '' } }
+
+      before { subject }
+
+      it 'returns 400' do
+        expect(response).to have_http_status 400
+      end
+    end
   end
 end
---
+++ spec/support/crawler_helper.rb
@@ -0,0 +1,7 @@
+def crawl_results
+  {
+    item_name: 'test',
+    item_price: '$123',
+    img_src: 'img_url'
+  }.to_json
+end
