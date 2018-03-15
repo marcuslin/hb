75259b03196f6c4e04078fc9d94eb3ecfe66c646
marcus lin <marcuslin0724@gmail.com>
2018-03-16 01:26:59 +0800

    update limit to 30

--- app/api/v1/search_item.rb
+++ app/api/v1/search_item.rb
@@ -7,7 +7,7 @@ module V1
       params do
         requires :key_word, allow_blank: false, type: String
         optional :sort_type, allow_blank: false, values: ['asc', 'desc'], default: 'asc'
-				optional :limit, type: Integer, default: 9999
+				optional :limit, type: Integer, default: 30
       end
 
       get '/', jbuilder: 'search/result' do
